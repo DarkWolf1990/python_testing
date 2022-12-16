57484f24573d19dd4ac5c49cfd83972c64cc2aa3
YOUR_NAME <YOUR_EMAIL>
2022-12-16 18:28:25 +0300

    task1

---
+++ HW_py/1.py
@@ -0,0 +1,27 @@
+
+# 1. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+# *Пример:*
+
+# - 6 -> да
+# - 7 -> да
+# - 1 -> нет
+
+print("Input day number of week  from 1 to 7")
+dayOfWeek=int(input())
+if (dayOfWeek==1):
+    print("Monday")
+elif (dayOfWeek==2):
+    print("Tuesday")
+elif(dayOfWeek==3):
+    print("Wednesday")
+elif(dayOfWeek==4):
+    print("Thursday")
+elif (dayOfWeek==5):
+    print("Friday")
+elif(dayOfWeek==6):
+    print("Saturday")
+elif(dayOfWeek==7):
+    print("Sunday")
+
+
